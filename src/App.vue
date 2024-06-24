c1d61b95d28111c1cbcac53a7ecb7221abd3d8b7
yakuphantonintshi <yakupha58756@gmail.com>
2024-06-24 11:50:43 +0200

    Revision

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+  <PropsExample numb1="66" numb2="12"/>
+  <form class="container">
+    <input type="text" placeholder="Enter your name" v-model="firstName">
+    <label>{{ output }}</label>
+    <button @click.prevent="greeting">Greeting</button>
+  </form>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PropsExample from '@/components/PropsExample.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PropsExample
+  },
+  data(){
+    return {
+      firstName: '',
+      output: ''
+    }
+  },
+  methods: {
+    greeting(){
+      this.output = `Hello ${this.firstName}`
+    }
   }
 }
 </script>
---
+++ src/components/PropsExample.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <p>
+        <span v-if="numb1>0">Numb1: {{ numb1 }}</span>
+        <span v-else>Enter numb1 value</span>
+    </p>
+
+    <p>
+        <span v-if="numb2 > 0">Numb2: {{ numb2 }}</span>
+        <span v-else>Enter a valid numb2 value</span>
+    </p>
+    <p>Sum: {{ addition }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["numb1", "numb2"],
+    computed:{
+        addition(){
+            return +this.numb1 + +this.numb2
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
